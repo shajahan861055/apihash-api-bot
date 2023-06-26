cbca1dd591806830b3bd6bade13b01ec6dfea137
shajahan861055 <137761792+shajahan861055@users.noreply.github.com>
2023-06-26 16:06:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/sinan-m-116/MyTelegramORG-bot.git /MyTelegramORG-bot
-cd /MyTelegramORG-bot
+git clone https://github.com/shajahan861055/apihash-api-bot
+cd/apihash-api-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
